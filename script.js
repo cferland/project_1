e10e5d80a312a746ecf56ed521ed1927776fe330
Corey Ferland <ferlandc@gmail.com>
2019-11-25 10:48:48 -0500

    add card functionality

--- script.js
+++ script.js
@@ -7,19 +7,43 @@ document.querySelector('button').addEventListener('click', async function (event
   let question = document.querySelector('#question-input').value;
   let update = 'One moment please...';
   document.querySelector('#question').innerHTML = `<p>${update}</p>`;
-  const result = await axios.get(`https://cors-anywhere.herokuapp.com/rws-cards-api.herokuapp.com/api/v1/cards/`);
-  document.querySelector('#question').innerHTML = `<p>${question}</p>`;
+  const result = await axios.get(`https://cors-anywhere.herokuapp.com/rws-cards-api.herokuapp.com/api/v1/cards/random?n=3`);
+  document.querySelector('#question').classList.add('hidden');
+  setTimeout(function () {
+    document.querySelector('#question').innerHTML = `<p>${question}</p>`;
+    document.querySelector('#question').classList.add('fade-in');
+  }, 1000);
   console.log(result);
   
-  // declare variables for each card object property
-  // display results on page
+  // display cards face-down on page
 
-  const cardName = result.data.cards[0].name;
-  const cardDesc = result.data.cards[0].meaning_up;
-  const cardRef = result.data.cards[0].name_short;
-  document.querySelector('.one .crop').innerHTML = `<img src="images/${cardRef}.jpg">`;
-  const cardContainer = document.createElement('div');
-  cardContainer.innerHTML = `<h3>${cardName}</h3><p>${cardDesc}</p>`;
-  document.querySelector('.card.one').appendChild(cardContainer);
+  setTimeout(function () {
+    document.querySelector('.one .card-back').classList.add('fade-in');
+  }, 1000);
+  setTimeout(function () {
+    document.querySelector('.two .card-back').classList.add('fade-in');
+  }, 2000);
+  setTimeout(function () {
+    document.querySelector('.three .card-back').classList.add('fade-in');
+  }, 3000);
+
+  // loop through cards and assign results to each
+  // display each card when clicked
+
+  const cardArray = document.querySelectorAll('.card');
+  for (let i = 0; i < cardArray.length; i++) {
+    const cardName = result.data.cards[i].name;
+    const cardDesc = result.data.cards[i].meaning_up;
+    const cardRef = result.data.cards[i].name_short;
+    const cardContainer = document.createElement('div');
+    cardContainer.classList.add('hidden');
+    cardContainer.innerHTML = `<h3>${cardName}</h3><p>${cardDesc}</p>`;
+    cardArray[i].querySelector('.crop').innerHTML = `<img src="images/${cardRef}.jpg">`;
+    cardArray[i].appendChild(cardContainer);  
+    cardArray[i].addEventListener('click', function (event) {
+      cardArray[i].querySelector('.crop').classList.add('fade-in');
+      cardContainer.classList.add('fade-in');
+    });
+  }
 
 })
